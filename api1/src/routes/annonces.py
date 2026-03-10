a9f9df1b8e8ba39f4e451327c5a9724b2ba03a56
Ascelefeu <cuinet.adrien@orange.fr>
2026-03-10 15:43:42 +0100

    ajout de routes

--- api1/src/routes/annonces.py
+++ api1/src/routes/annonces.py
@@ -1,15 +1,33 @@
 # routes/annonces.py
 from fastapi import APIRouter
-from ..schemas import AnnonceCreate
+from ..schemas import AnnonceCreate, AnnonceResponse
+from ..database import database
+from datetime import datetime
+
 router = APIRouter()
 
 @router.get("/annonces")
-async def lister_annonces():
-    return [...]
+async def lister_annonces() -> list[AnnonceResponse]:
+    collection = database["annonces"]
+    cursor = collection.find({"is_active": True})
+    annonces = await cursor.to_list(length=100)
+    for annonce in annonces:
+        annonce["id"] = str(annonce.pop("_id"))
+    return annonces
 
 @router.post("/annonces")
-async def creer_annonce(annonce: AnnonceCreate):
-    return {...}
+async def creer_annonce(annonce: AnnonceCreate) -> AnnonceResponse:
+    collection = database["annonces"]
+    
+    doc = annonce.dict()
+    doc["date_post"] = datetime.now()
+    doc["is_active"] = True
+    
+    result = await collection.insert_one(doc)
+    
+    doc["id"] = str(result.inserted_id)
+    
+    return AnnonceResponse(**doc)
 
 @router.get("/annonces/{annonce_id}")
 async def obtenir_annonce(annonce_id: str):
@@ -22,4 +40,7 @@ async def mettre_a_jour_annonce(annonce_id: str, annonce: AnnonceCreate):
 @router.delete("/annonces/{annonce_id}")
 async def supprimer_annonce(annonce_id: str):
     return {...}
+
+
+
 
